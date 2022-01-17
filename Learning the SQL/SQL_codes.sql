4b31c88f5b26c8056e3dd5c4d7a70b35a6b7e0df
Parsa KamaliPour <parsakamalipour@gmail.com>
2022-01-17 21:26:18 +0330

    sql init

---
+++ Learning the SQL/SQL_codes.sql
@@ -0,0 +1,13 @@
+-- creating a new table
+
+CREATE TABLE Students (
+	sid CHAR(20),
+	name CHAR(20),
+	login CHAR(10),
+	age INTEGER,
+	gpa REAL
+);
+
+-- destroying that table
+
+DROP TABLE Students;
