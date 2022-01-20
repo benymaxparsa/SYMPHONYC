20e3e447e0338e6084c54842ff6e650819e7b6be
Parsa KamaliPour <parsakamalipour@gmail.com>
2022-01-20 16:40:16 +0330

    Added LikedAlbums, Follows, FavoriteArtists, Playlists, FollowedPlaylists

--- SQL Codes of SYMPHONYC/TABLES.sql
+++ SQL Codes of SYMPHONYC/TABLES.sql
@@ -38,5 +38,52 @@ CREATE TABLE Albums(
 	CONSTRAINT Albums_FK FOREIGN KEY (artist_username) REFERENCES Artists (username)
 );
 
+CREATE TABLE LikedAlbums(
+	username CHARACTER VARYING,
+	album_id CHARACTER VARYING,
+	CONSTRAINT LikedAlbums_PK PRIMARY KEY (album_id, username),
+	CONSTRAINT LikedAlbums_FK1 FOREIGN KEY (username) REFERENCES NormalUsers (username),
+	CONSTRAINT LikedAlbums_FK2 FOREIGN KEY (album_id) REFERENCES Albums (album_id)
+);
+
+CREATE TABLE Follows(
+	follower_username CHARACTER VARYING,
+	following_username CHARACTER VARYING,
+	date_of_following DATE,
+	CONSTRAINT Follows_PK PRIMARY KEY (follower_username, following_username),
+	CONSTRAINT Follows_FK1 FOREIGN KEY (following_username) REFERENCES NormalUsers (username),
+	CONSTRAINT Follows_FK2 FOREIGN KEY (follower_username) REFERENCES NormalUsers (username)
+);
+
+CREATE TABLE FavoriteArtists(
+	person_username CHARACTER VARYING,
+	artist_username CHARACTER VARYING,
+	date_of_following DATE,
+	CONSTRAINT FavoriteArtists_PK PRIMARY KEY (person_username, artist_username),
+	CONSTRAINT FavoriteArtists_FK1 FOREIGN KEY (person_username) REFERENCES NormalUsers (username),
+	CONSTRAINT FavoriteArtists_FK2 FOREIGN KEY (artist_username) REFERENCES Artists (username)
+);
+
+CREATE TABLE Playlists(
+	playlist_id CHARACTER VARYING,
+	creator_username CHARACTER VARYING,
+	title CHARACTER VARYING,
+	number_of_followers INTEGER,
+	description CHARACTER VARYING,
+	image_url CHARACTER VARYING,
+	CONSTRAINT Playlists_PK PRIMARY KEY (playlist_id),
+	CONSTRAINT Playlists_FK FOREIGN KEY (creator_username) REFERENCES NormalUsers (username)
+);
+
+CREATE TABLE FollowedPlaylists(
+	username CHARACTER VARYING,
+	playlist_id CHARACTER VARYING,
+	user_given_score DOUBLE PRECISION,
+	CONSTRAINT FollowedPlaylists_PK PRIMARY KEY (playlist_id, username),
+	CONSTRAINT FollowedPlaylists_FK1 FOREIGN KEY (username) REFERENCES NormalUsers (username),
+	CONSTRAINT FollowedPlaylists_FK2 FOREIGN KEY (playlist_id) REFERENCES Playlists (playlist_id),
+	CONSTRAINT user_given_score_limit CHECK (user_given_score BETWEEN 0.0 AND 5.0)
+);
+
 
 
